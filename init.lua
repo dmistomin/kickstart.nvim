2dfabfdb1e6c39eba94b0175a8ed18b042bff0bf
dmistomin <distomin+git@pm.me>
2026-03-05 12:52:46 +0900

    swap colors to Kanagawa color scheme

--- init.lua
+++ init.lua
@@ -806,20 +806,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      require('kanagawa').setup {
+        commentStyle = { italic = false },
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Available styles: 'kanagawa-wave' (default), 'kanagawa-dragon' (darker), 'kanagawa-lotus' (light)
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
